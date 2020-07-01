e0d67083f001c6d382a33efb023b6f67e8e5ad67
Sese <sese.miguel9@gmail.com>
2020-07-01 22:50:52 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Item from './components/Item.js'
+
+function App(){
+    return(
+        <>
+        <Item />
+        <Item />
+        <Item />
+        </>
+    )
+}
+
+export default App;
---
+++ src/components/Item.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Item(){
+    return(
+        <div>
+            <input type="checkbox"></input>
+            Check box 
+        </div>
+        
+    );
+}
+
+export default Item;
