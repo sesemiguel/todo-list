6826269dc1f8d8059a75869e925cdb8deeb158c1
Sese <sese.miguel9@gmail.com>
2020-07-05 13:38:02 +0800

    changed setting of checkbox attributes, added onChange attribute

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,28 +3,16 @@ import React from "react"
 class Item extends React.Component {
 
     render() {
-        const isDone = () => {
-            if (this.props.item.completed === true) {
-                return (
-                    <>
-                        <input type="checkbox" checked="true" /> <s>{this.props.item.text} </s>
-                        <br />
-                    </>
-                )
-            }
-            else {
-                return (
-                    <>
-                        <input type="checkbox" /> {this.props.item.text}
-                        <br />
-                    </>
-                )
-            }
-        }
 
         return (
             <>
-                {isDone()}
+                <input 
+                    type="checkbox" 
+                    checked={this.props.item.completed} 
+                    onChange={() => console.log("This has been changed")}
+                />
+                <p>{this.props.item.text}</p>
+                <br />
             </>
         )
     }
